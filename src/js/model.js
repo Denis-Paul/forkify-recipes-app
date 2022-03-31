bff30439590c938a3af870555e7bf8ff541410e4
Denis Paul <denis.paul@softvision.com>
2022-03-31 11:52:11 +0300

    implemented error and success messages

--- src/js/model.js
+++ src/js/model.js
@@ -24,6 +24,7 @@ export const loadRecipe = async function(id) {
         console.log(state.recipe);
     } catch (err) {
         // alert(err);
-        console.error(`${err} !!!`); // temp error handling
+        // console.error(`${err} !!!`); // temp error handling
+        throw err;
     }
 }
