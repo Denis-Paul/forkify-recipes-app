258f8c7f0f3fe0a063c1f49d205d92b63fb3a285
Denis Paul <denis.paul@softvision.com>
2022-04-04 15:37:15 +0300

    example of adding JS doc for functions

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,16 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
     _data;
     
+    /**
+     * Render the received object to the DOM
+     * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+     * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+     * @returns {undefined | string} A markup string is returned if render=false
+     * @this {Object} View instance
+     * @author Denis Paul
+     * @todo Finish implementation
+     */
+    // find more information about JSDoc at: https://jsdoc.app/
     render(data, render = true) {
         if (!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
 
