9c25b8167f3d7faf7393d6b2e78d3a61e6ff0fdd
Denis Paul <denis.paul@softvision.com>
2022-04-04 15:28:09 +0300

    added new recipe upload functionality, refactoring

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'cd3563b0-d2b2-4620-bcf8-ae387b421d91';
+export const MODAL_CLOSE_SEC = 2.5;
